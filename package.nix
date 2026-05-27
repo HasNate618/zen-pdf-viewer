969d3001780f01dfb1b623441a7575da0f5e50e2
HasNate618 <nate.e.espejo@gmail.com>
2026-05-27 18:03:18 -0400

    fix: Make binary executable

--- package.nix
+++ package.nix
@@ -32,6 +32,7 @@ stdenv.mkDerivation {
     mkdir -p "$out/share/zen-pdf-viewer" "$out/bin"
     cp viewer.html "$out/share/zen-pdf-viewer/"
     cp launch.sh "$out/bin/zen-pdf-viewer"
+    chmod +x "$out/bin/zen-pdf-viewer"
 
     wrapProgram "$out/bin/zen-pdf-viewer" \
       --set ZEN_PDF_VIEWER_DATA_DIR "$out/share/zen-pdf-viewer" \
